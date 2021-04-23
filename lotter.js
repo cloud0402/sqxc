3d58bcbc1bc84fb9db1b6d4bd3043dd38bfd0c03
Cloud.Y <cloud.y.r.s@gmail.com>
2021-04-23 09:56:32 +0800

    Update lotter.js

--- lotter.js
+++ lotter.js
@@ -64,10 +64,9 @@ function getData() {
     )
 }
 
-const resulturl = "https://www.mxnzp.com/api/lottery/common/check?code=ssq&expect=" + ssqDataAnalyzed.expect
-    + "&lotteryNo=03,04,06,10,18,21,33@10,16&app_id=tzyjqeqgqksqpnmp&app_secret=TWxoOWZkamlWdDhSeTBWcTBiRTkwdz09";
-
 function getResult() {
+    const resulturl = "https://www.mxnzp.com/api/lottery/common/check?code=ssq&expect=" + ssqDataAnalyzed.expect
+        + "&lotteryNo=03,04,06,10,18,21,33@10,16&app_id=tzyjqeqgqksqpnmp&app_secret=TWxoOWZkamlWdDhSeTBWcTBiRTkwdz09";
     $task.fetch(resulturl).then(response => {
             // let ssqData = JSON.parse(response.body);
             // ssqDataAnalyzed.openCode = ssqData.data.openCode;
