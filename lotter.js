f76f73e1abdd749dd3cc83f879e8fdee2c550453
Cloud.Y <cloud.y.r.s@gmail.com>
2021-04-23 09:24:53 +0800

    增加lotter

---
+++ lotter.js
@@ -0,0 +1,40 @@
+/**
+ * 查询双色球中奖信息
+ *
+ */
+
+const url = "https://www.mxnzp.com/api//lottery/common/latest?code=ssq&app_id=tzyjqeqgqksqpnmp&app_secret=TWxoOWZkamlWdDhSeTBWcTBiRTkwdz09";
+const method = "POST";
+const headers = {
+    // "Accept": "*/*",
+    // "Accept-Encoding": "br, gzip, deflate",
+    // "Accept-Language": "zh-cn",
+    // "Connection": "close",
+    // "Content-Type": "application/x-www-form-urlencoded",
+    // "Host": "app.yymedias.com",
+    // "Origin": "https://apph5.yymedias.com",
+    // "Referer": "https://apph5.yymedias.com/app/mission",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16A404",
+    // "header-encrypt-code": "123456788sY3zv+fnn5mcDFP6LZBAbmEo=",
+    // "is-ios": "ios"
+};
+const data = {"info": "abc"};
+
+const myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("Title", "Subtitle", response.body); // Success!
+    $done();
+}, reason => {
+    // reason.error
+    $notify("Title", "Subtitle", reason.error); // Error!
+    $done();
+});
+
