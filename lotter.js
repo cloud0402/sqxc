e2b14282ad00f69bd681f9b9a246d6cd3e2afd4a
Cloud.Y <cloud.y.r.s@gmail.com>
2021-04-23 14:49:58 +0800

    更新算法

--- lotter.js
+++ lotter.js
@@ -1,100 +1,103 @@
 /**
  * 查询双色球中奖信息
- *  {"code":1,"msg":"数据返回成功！","data":{"openCode":"02,03,17,18,23,24+01","code":"ssq","expect":"2021043","name":"双色球","time":"2021-04-22 21:18:20"}}
- *   '{"code":"1","msg":"ok","data":"123"}';
- *   '{"result":true, "count":42}'
- *
- */
+ **/
 
 const url = "https://www.mxnzp.com/api//lottery/common/latest?code=ssq&app_id=tzyjqeqgqksqpnmp&app_secret=TWxoOWZkamlWdDhSeTBWcTBiRTkwdz09";
 
 const method = "POST";
-const headers = {
-    // "Accept": "*/*",
-    // "Accept-Encoding": "br, gzip, deflate",
-    // "Accept-Language": "zh-cn",
-    // "Connection": "close",
-    // "Content-Type": "application/x-www-form-urlencoded",
-    // "Host": "app.yymedias.com",
-    // "Origin": "https://apph5.yymedias.com",
-    // "Referer": "https://apph5.yymedias.com/app/mission",
-    // "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16A404",
-    // "header-encrypt-code": "123456788sY3zv+fnn5mcDFP6LZBAbmEo=",
-    // "is-ios": "ios"
-};
-const data = {"info": "abc"};
 
 const myDataRequest = {
     url: url,
     method: method, // Optional, default GET.
-    // headers: headers, // Optional.
-    // body: JSON.stringify(data) // Optional.
-};
-
-const myCashRequest = {
-    url: url,
-    method: method, // Optional, default GET.
-    headers: headers, // Optional.
-    body: JSON.stringify(data) // Optional.
 };
 
 var openCode = "";
+var checkedCode = "";
 var expect = "";
 var time = "";
-var ssqresult = "";
-var cashresult = "";
+var Title = "";
+var SubTitle = "";
+var content = "";
 
 function getData() {
     $task.fetch(myDataRequest).then(response => {
-            var ssqData = JSON.parse(response.body);
+            ssqData = JSON.parse(response.body);
             openCode = ssqData.data.openCode.toString();
             expect = ssqData.data.expect.toString();
             time = ssqData.data.time.toString();
-
-            // response.statusCode, response.headers, response.body
-            // console.log(response.body);
-            // $notify("Title", "Subtitle", expect); // Success!
-            // $notify("Title", "Subtitle", response.body); // Success!
-            // $done();
+            return response;
         }, reason => {
             // reason.error
             $notify("Title", "Subtitle", reason.error); // Error!
             $done();
         }
-    )
-}
+    ).then(function (getResult) {
+            var resulturl = "https://www.mxnzp.com/api/lottery/common/check?code=ssq&expect=" + expect
+                + "&lotteryNo=03,04,06,10,18,21,33@10,16&app_id=tzyjqeqgqksqpnmp&app_secret=TWxoOWZkamlWdDhSeTBWcTBiRTkwdz09";
+            var myCashRequest = {
+                url: resulturl,
+                method: method, // Optional, default GET.
+            };
+            $task.fetch(myCashRequest).then(response => {
+                    ssqData = JSON.parse(response.body);
+                    resultDesc = ssqData.data.resultDesc;
+                    checkedCode = ssqData.data.checkedCode;
 
-function getResult() {
-    var resulturl = "https://www.mxnzp.com/api/lottery/common/check?code=ssq&expect=" + expect
-        + "&lotteryNo=03,04,06,10,18,21,33@10,16&app_id=tzyjqeqgqksqpnmp&app_secret=TWxoOWZkamlWdDhSeTBWcTBiRTkwdz09";
-    // $notify("Title", "Subtitle", resulturl);
-    var myCashRequest = {
-        url: resulturl,
-        method: method, // Optional, default GET.
-        // headers: headers, // Optional.
-        // body: JSON.stringify(data) // Optional.
-    };
-    $task.fetch(myCashRequest).then(response => {
-            // let ssqData = JSON.parse(response.body);
-            // ssqDataAnalyzed.openCode = ssqData.data.openCode;
-            // ssqDataAnalyzed.expect = ssqData.data.expect;
-            // ssqDataAnalyzed.time = ssqData.data.time;
-
-            // response.statusCode, response.headers, response.body
-            // console.log(response.body);
-            $notify("Title", "Subtitle", response.body); // Success!
-            $done();
-        },reason => {
-            // reason.error
-            $notify("Title", "Subtitle", reason.error); // Error!
-            $done();
+                    redResult = resultDesc.substring(0, 1);
+                    blueResult = resultDesc.substring(2, 3);
+                    // ssqDataAnalyzed.openCode = ssqData.data.openCode;
+                    // ssqDataAnalyzed.expect = ssqData.data.expect;
+                    // ssqDataAnalyzed.time = ssqData.data.time;
+                    if (blueResult === "0") {
+                        if (redResult === "6") {
+                            Title = "二等奖";
+                            SubTitle = "具体奖金请查询";
+                        } else if (redResult === "5") {
+                            Title = "四等奖";
+                            SubTitle = "中奖奖金为：900元";
+                        } else if (redResult === "4") {
+                            Title = "五等奖";
+                            SubTitle = "中奖奖金为：60元";
+                        } else {
+                            Title = "未中奖";
+                            SubTitle = "未中奖";
+                        }
+                    } else if (blueResult === "1") {
+                        if (redResult === "6") {
+                            Title = "一等奖";
+                            SubTitle = "具体奖金请查询";
+                        } else if (redResult === "5") {
+                            Title = "三等奖";
+                            SubTitle = "中奖奖金为：7450元";
+                        } else if (redResult === "4") {
+                            Title = "四等奖";
+                            SubTitle = "中奖奖金为：670元";
+                        } else if (redResult === "3") {
+                            Title = "五等奖";
+                            SubTitle = "中奖奖金为：55元";
+                        } else {
+                            Title = "六等奖";
+                            SubTitle = "中奖奖金为：35元";
+                        }
+                    }
+                    content = "第" + expect + "期双色球开奖结果\n"
+                    content += "开奖时间："+ time + "\n";
+                    content += "开奖结果："+ openCode + "\n";
+                    content += "你的投注："+ checkedCode + "\n";
+                    content += "共中 "+ redResult + " 个红球，" + blueResult + "个蓝球\n";
+                    content += SubTitle + "\n";
+                    // response.statusCode, response.headers, response.body
+                    // console.log(response.body);
+                    $notify(Title, SubTitle, content); // Success!
+                    $done();
+                }, reason => {
+                    // reason.error
+                    $notify("Title", "Subtitle", reason.error); // Error!
+                    $done();
+                }
+            )
         }
     )
 }
 
-function ssq() {
-    getData();
-    getResult();
-}
-
-ssq();
+getData();
